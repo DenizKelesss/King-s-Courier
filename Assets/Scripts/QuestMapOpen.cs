1554d354d327c874790c67ff77fe5e8c65da9b23
DenizKelesss <denizkelessss@gmail.com>
2024-07-16 22:43:54 +0300

    quest map implemented + kenney asset removed
    quest map needs work, but reputation is being saved between levels

---
+++ Assets/Scripts/QuestMapOpen.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestMapOpen : MonoBehaviour
+{
+
+    private bool isPlayerInRange = false;
+    public GameObject QuestMap;
+
+    private void Update()
+    {
+        if(isPlayerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            QuestMap.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInRange = true;
+        }
+    }
+
+}
