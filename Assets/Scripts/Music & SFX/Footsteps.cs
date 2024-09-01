ea13c7fddd7900d11e55d44c6761df33c366ad36
DenizKelesss <denizkelessss@gmail.com>
2024-09-02 00:29:15 +0300

    new footstep system + new npcs + build 0.2.8

--- Assets/Scripts/Music & SFX/Footsteps.cs
+++ Assets/Scripts/Music & SFX/Footsteps.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class Footsteps : MonoBehaviour
 {
     public AudioSource footStepsSource;
+    private bool isGrounded = true;
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        if (isGrounded && Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
         { 
             footStepsSource.enabled = true;
         }
@@ -20,6 +21,22 @@ public class Footsteps : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             footStepsSource.enabled = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true; // Player is grounded
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false; // Player is grounded
         }
     }
 }
